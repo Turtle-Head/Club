73b2bd95d0d646faccaefeab3177a4aba8bce65f
James Fehr <fehr.james@gmail.com>
2023-01-25 18:43:34 -0800

    updates

--- ICR100/ICR300/25-23/js/script.js
+++ ICR100/ICR300/25-23/js/script.js
@@ -1,20 +1,20 @@
-// let moreInputs = "Y";
-// let largest = "0";
-// while (moreInputs === "Y") {
-//     let userInput = +prompt("Please enter a number to compare for highest", "1");
-//     if (largest < userInput) {
-//         largest = userInput;
-//     }
-//     moreInputs = prompt("Are there any more inputs? Y or N", "Y")
-// }
-// alert(`The largest number is ${largest}`)
-let num1=1;
-let num2=9;
-let result=0;
-for(let i=num1, i < num2,i++){
-    if(count%2==0){
-        console.log(i);
-        result=result+i;
+let moreInputs = "Y";
+let largest = "0";
+while (moreInputs === "Y") {
+    let userInput = +prompt("Please enter a number to compare for highest", "1");
+    if (largest < userInput) {
+        largest = userInput;
     }
-    console.log(`the sum is `+result)
+    moreInputs = prompt("Are there any more inputs? Y or N", "Y")
 }
+alert(`The largest number is ${largest}`)
+// let num1=1;
+// let num2=9;
+// let result=0;
+// for(let i=num1, i < num2,i++){
+//     if(count%2==0){
+//         console.log(i);
+//         result=result+i;
+//     }
+//     console.log(`the sum is `+result)
+// }
