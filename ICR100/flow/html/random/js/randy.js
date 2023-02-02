f8513b3bb2a4d087fa1d1268213abe45cd22a0bb
James Fehr <fehr.james@gmail.com>
2023-02-02 15:19:23 -0800

    doing some hacking at code I design

--- ICR100/flow/html/random/js/randy.js
+++ ICR100/flow/html/random/js/randy.js
@@ -1,37 +1,46 @@
 // Random number generator
 function randnums(){
-    let number = Math.floor(Math.random() * 1000);
-    // let num2 = Math.floor(Math.random() * 9);
-    console.log(`this ${number} is random`);
-    // console.log(`this ${num2} is random`);
-    console.log(number);
+    let number = Math.floor(Math.random() * 100);
+    // returns a random number
     return number;
 }
 // gets 100 random numbers
-let arRandy =[];
-for(let i=0;i<100;i++) {
-    arRandy.push[randnums()];
+const arRand = [];
+for(let i = 0;i < 100; i++) {
+    arRand.push(randnums());
 }
-console.log(arRandy);
+// the array of randoms
+console.log(`Array: ${arRand}`);
+
+// Gets Min number in random number array the "..." indicate to use the array next to it
+console.log(`Min result:  ${Math.min(...arRand)}`); 
+// Gives Max number in random number array the "..." indicate to use the array next to it 
+console.log(`Max result: ${Math.max(...arRand)}`);    // the ... makes it work!!!!!
+
+
+
 // Takes in 2 numbers
 // checks if even or odd
 // some addition between both user numbers and a counter
-function addifeven(){
-    let num1=+prompt("1");
-    let num2=+prompt("9")
+function addifeven(num1, num2){
+    // let num1=+prompt("1");
+    // let num2=+prompt("9")
     let result=0;
     for (let i = num1; i < num2; i++){
         if(i%2==0){
-            console.log(i);
+            // console.log(i);
             result+=i;
-            alert(${i})
+            // alert(${i})
         }
-        console.log(`the sum is ${result}`)
+        // console.log(`the sum is ${result}`)
         
     }
-    alert(`the sum is ${result}`)
+    // console.log(`the sum is ${result}`)
 }
-addifeven();
+// for(let i=0;i<100;i++){
+//     addifeven(arRandy[i],arRandy[i+1]);
+// }
+
 // Multiplication
 // takes in 2 numbers
 function mult(xn,yn){
@@ -42,33 +51,34 @@ function mult(xn,yn){
     }
     alert(`${total} is the multiplied values between ${xn} and ${yn}`);
 }
-mult(9,19);
+// mult(9,19);
 //Division
 // Takes in 2 numbers
 function divit(xn,yn){
     let total = 0;
     for(let c = xn; c <= yn; c++){
         total = c/yn;
-        console.log(`${total} is the result for ${yn} and ${xm}`);
+        console.log(`${total} is the result for ${yn} and ${xn}`);
     }
-    alert(`${total} is the result of ${xm} and ${yn}`);
+    // alert(`${total} is the result of ${xn} and ${yn}`);
 }
-divit(5,69);
+// divit(5,69); calls divit
+//
 //setting up sum of numbers function
 function stackofnum(ynum,xnum) { 
     let sum = 0;  //initializing sum to hold value of calculations
     for (let a = ynum; a <= xnum; a++) { //takes in value of number to be added up to and including
       // console.log(a); //displays index to ensure viability of code
       sum += a;   //adds index to value of sum
-      console.log(sum);   //displays sum
+    //   console.log(sum);   //displays sum
     }
-    alert(`${sum} is the sum of
-      ${ynum} 
-      up to 
-      ${xnum}`);   // displays sum of numbers
+    // alert(`${sum} is the sum of
+    //   ${ynum} 
+    //   up to 
+    //   ${xnum}`);   // displays sum of numbers
   }
-  stackofnum(1,100);    //calling the addition function with value of 100
-  stackofnum(-32,-6);
+//   stackofnum(1,100);    //calling the addition function with value of 100
+//   stackofnum(-32,-6);
   // hello world function
 function hello(greet, name){  //takes in a name
     console.log(greet,name);     // displays messsage validating code viability
